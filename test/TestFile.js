63903417f680d652ae5dd6b8e3eb1ac0acb866dd
Mo Omer <beancinematics@gmail.com>
2015-03-06 13:21:38 -0600

    Remove test folder

--- test/TestFile.js
+++
@@ -1,9 +0,0 @@
-(function () { "use strict";
-var test = {};
-test.TestFile = function() { };
-test.TestFile.main = function() {
-	var myList = Titanium.UI.createLabel({ color : "#999", text : "I am Window 2", font : { fontSize : 20, fontFamily : "Helvetica Neue"}, textAlign : "center", width : "auto"});
-	console.log("Hello World; test passed.");
-};
-test.TestFile.main();
-})();
